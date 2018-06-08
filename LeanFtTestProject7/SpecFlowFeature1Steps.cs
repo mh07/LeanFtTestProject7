7888a7f9ce7ef4a60b41026a1011b0413df8a028
Henry <mason.henry@hpe.com>
2018-06-08 15:35:42 -0400

    Add project files.

---
+++ LeanFtTestProject7/SpecFlowFeature1Steps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using HP.LFT.SDK.Web;
+
+namespace LeanFtTestProject7
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have entered (.*) into the calculators")]
+        public void GivenIHaveEnteredIntoTheCalculators(int p0)
+        {
+            //  ScenarioContext.Current.Pending();
+            var browser = BrowserFactory.Launch(BrowserType.Chrome);
+
+        }
+        
+        [When(@"I press addd")]
+        public void WhenIPressAddd()
+        {
+         //   ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be (.*) on the screeb")]
+        public void ThenTheResultShouldBeOnTheScreeb(int p0)
+        {
+         //   ScenarioContext.Current.Pending();
+        }
+    }
+}
